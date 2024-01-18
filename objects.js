f3d94c4ad98d66f025e067f53adc6cc64f44b64a
Darshan Bashetti <104633319+Darshanbashetti@users.noreply.github.com>
2024-01-18 18:43:58 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,13 @@
+<script>
+  const darshan = {
+    firstname: "darshan",
+    lastname: "bashetti",
+    age: 2024 - 2001,
+    frds: ["vonya", "shashi", "niks"],
+
+    function: abcd(value),
+  };
+  console.log(darshan.firstname);
+  const abcd = prompt("Choose between firstname, lastname, age, frds");
+  console.log(darshan[abcd]);
+</script>
